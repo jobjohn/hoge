b9e03ce577836b64758772c709b3c399ef79d561
Mossuru777 <mossuru777@gmail.com>
2020-07-01 23:39:33 +0900

    Add original script

---
+++ test.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.create_widgets()
+
+    def create_widgets(self):
+
+        fr1 = tk.Frame(root)
+        fr1.pack()
+
+        selected_frame = tk.IntVar()
+        rd1 = tk.Radiobutton(fr1, text='DLsite',
+                             variable=selected_frame, value=0)
+        # rd1.pack(fill='x', side='left')
+        rd1.pack()
+        rd2 = tk.Radiobutton(
+            fr1, text='Fansa', variable=selected_frame, value=1)
+        # rd2.pack(fill='x', side='left')
+        rd2.pack()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    myapp = Application(master=root)
+    myapp.master.title("ファイル名変換くん")  # タイトル
+    myapp.master.geometry("700x200")  # ウィンドウの幅と高さピクセル単位で指定 (width x height)
+
+    myapp.mainloop()
